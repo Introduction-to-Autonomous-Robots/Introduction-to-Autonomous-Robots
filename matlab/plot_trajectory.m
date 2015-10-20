495f13d25a8cf0b2f8c1c1455e4bbe7b98ca1847
Nikolaus Correll <ncorrell@colorado.edu>
2015-10-19 20:15:41 -0600

    error propagation matlab code added

--- matlab/plot_trajectory.m
+++ matlab/plot_trajectory.m
@@ -1,19 +1,29 @@
 % Generate wheel movements
 dsl=ones(100,1)*0.11;
-dsr=ones(100,1)*0.10;
+dsr=ones(100,1).*0.10;
 
 % Initialize position
 x=0;
 y=0;
 theta=0;
-Cp=[0.01 0 0; 0 0.01 0; 0 0 0.01];
+Cp=[0 0 0; 
+    0 0 0; 
+    0 0 0];
 
 % Setup drawing
+subplot(1,2,2);
+set(gcf,'DoubleBuffer','On');
+xlabel('X position');
+ylabel('Y positiion');
+
+subplot(1,2,1);
 hold on;
 set(gcf,'DoubleBuffer','On');
 axis([0 12 -2 7]);
 xlabel('X position');
 ylabel('Y positiion');
+
+
 
 for I=1:length(dsl),
  oldx=x;
@@ -21,6 +31,7 @@ for I=1:length(dsl),
  [x,y,theta,Cp]=pos_update(x,y,theta,dsl(I),dsr(I),Cp);
  
  % Plotting
+ subplot(1,2,1);
  plot([oldx x],[oldy y],'b--');
  if(mod(I,10)==0)
   quiver(x,y,cos(theta),sin(theta),'b');
@@ -28,6 +39,10 @@ for I=1:length(dsl),
   Ellipse_x=Cp(1,1).*cos(0:0.01:2*pi);
   Ellipse_y=Cp(2,2).*sin(0:0.01:2*pi);
   plot(x+Ellipse_x.*cos(theta)-Ellipse_y*sin(theta),y+Ellipse_x.*sin(theta)+Ellipse_y*cos(theta),'--');
+
  end;
- drawnow;
+ subplot(1,2,2);
+  G=Gauss2D(repmat(linspace(0,12),100,1),repmat(linspace(-2,7),100,1)',x,y,Cp(1,1),Cp(2,2)); 
+  mesh(linspace(0,12),linspace(-2,7),G);
+  drawnow;
 end;
