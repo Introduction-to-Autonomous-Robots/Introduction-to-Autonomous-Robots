ddbb55d3bf5f70955c3a95991dd7079cbfc95687
Nikolaus Correll <ncorrell@colorado.edu>
2015-11-03 10:24:23 -0700

    bugfix in matlab example for linear combinations of gaussians

--- matlab/linear_combinations_of_Gaussians.m
+++ matlab/linear_combinations_of_Gaussians.m
@@ -1,17 +1,20 @@
 m1=5;
-s1=2.1;
+s1=1;
 
-m2=12;
-s2=1;
+m2=6;
+s2=8;
 
 d1=normrnd(m1,s1,10000,1);
 d2=normrnd(m2,s2,10000,1);
 
 m=m1+s1^2/(s1^2+s2^2)*(m2-m1)
-s=1/(1/s1^2+1/s2^2)
+s=sqrt(1/(1/s1^2+1/s2^2))
 
 
 d3=normrnd(m,s,10000,1);
 
 hist([d1 d2 d3],50)
 legend('\sigma_1','\sigma_2','\sigma')
+%hist([d1 d2],50)
+%legend('\sigma_1','\sigma_2')
+
