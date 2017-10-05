2eb825da7021bdc06517631d0ed2c86bbac177fe
Nikolaus Correll <ncorrell@colorado.edu>
2017-10-05 17:43:59 -0600

    improved matlab commentary

--- matlab/die_experiment.m
+++ matlab/die_experiment.m
@@ -1,7 +1,31 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Introduction to Autonomous Robots
+%       Nikolaus Correll
+%
+% die_experiment.m
+%
+% Goal: help you understand the concept of
+% a discrete probability distribution function
+% and the limits of the Gaussian distribution.
+%
+% Activities: run the program either with the 
+% single die experiment for different numbers 
+% of iterations (I). The histogram function 
+% counts the number of occurences of the value
+% your die/dice return. How do you expect this
+% distribution to look like for more iterations?
+% Will it be Gaussian? What else? 
+
 N=100;
-for I=1:1000,
-%histogram(randi([1 6],1,N*I),6)
-histogram(randi([1 6],1,N*I)+randi([1 6],1,N*I),11)
+for I=1:10,
+%% Single die experiment (comment/uncomment either one)   
+histogram(randi([1 6],1,N*I),6)
+%% Double dice experiment
+%histogram(randi([1 6],1,N*I)+randi([1 6],1,N*I),11)
+
 title(sprintf('Number of samples %d',N*I));
+xlabel('Value');
+ylabel('Occurences');
 drawnow
 end;
--- matlab/feedbackcontrol.m
+++ matlab/feedbackcontrol.m
@@ -1,3 +1,25 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Introduction to Autonomous Robots
+%       Nikolaus Correll
+%
+% feedbackcontrol.m
+%
+% Goal: understand the impact different gains
+% have during feedback control and how they 
+% affect stability. 
+%
+% Activities: Play with different initial positions
+% of the robot (X) and goals (G) to observe the 
+% trajectories the controller generates. Now change 
+% the different gains (s, a, and e) by slightly varying
+% them up and down. Does the system behave as expected?
+% Now set the maximum speed of the robot to infinity (Inf)
+% Use conservative values to start (0.1,1,0.01), then
+% start increasing them. When does the behavior of
+% the robot become instable? Why? 
+
+
 s=0.5;  % Proportional gain for forward speed
 a=1;    % Proportional gain for rotational speed
 e=0.01; % Proportional gain to adjust robot's final orientation
@@ -5,7 +27,7 @@ e=0.01; % Proportional gain to adjust robot's final orientation
 
 X=[0;0;0];       % Initial position of robot (x,y,theta)
 G=[-0.1;-0.1;0]; % Goal position of robot
-maxspeed=0.0285; % Maximum speed the robot can drive
+maxspeed=0.0285; % Maximum speed the robot can drive, set to Inf if not needed
 
 alpha=Inf; rho=Inf; eta=Inf;
 figure(1); hold on; set(gcf,'DoubleBuffer','On');
